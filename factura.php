cbac452d6c1b76b2a0a27dc1aad15dbefca20d30
MICHELL IBARRA <150400153+Michell0212@users.noreply.github.com>
2025-07-15 08:48:05 -0500

    Update factura.php

--- factura.php
+++ factura.php
@@ -28,7 +28,7 @@ $total_general = 0;
   <meta charset="UTF-8">
   <title>Factura Generada</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../css/styles.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <div class="container mt-5 p-4 bg-white rounded shadow">
